360d46817d1bc2b122927cb36ffebc6bbdb26ba1
Tobias Olry <tobias.olry@web.de>
2015-03-07 11:36:23 +0100

    improve in-page navigation a bit

--- source/js/app.js
+++ source/js/app.js
@@ -6,15 +6,24 @@ $(document).ready(function() {
     var idCounter = 1;
     var $ul       = $('.toc ul');
 
-    $('h2,h3').each(function() {
-        var id = "toc_entry_" + idCounter;
-        var indent = (this.tagName == 'H2') ? 0 : 1;
+    $('h2,h3')
+        .each(function() {
+            var id = "toc_entry_" + idCounter;
+            var indent = (this.tagName == 'H2') ? 0 : 1;
 
-        $(this).attr('id', id);
+            var $li = $('<li><a href="#' + id + '">' + '&nbsp;'.repeat(indent * 4) + $(this).html() + '</a></li>');
+            $ul.append($li);
 
-        var $li = $('<li><a href="#' + id + '">' + '&nbsp;'.repeat(indent * 4) + $(this).html() + '</a></li>');
-        $ul.append($li);
+            $(this).attr('id', id);
+            $(this).data('nav-link', $li);
 
-        idCounter++;
-    });
+            idCounter++;
+        });
+
+    $('.nav-sidebar a')
+        .on('click', function() {
+            $('.nav-sidebar li.active').removeClass('active');
+            $(this).parent().addClass('active');
+            console.log('activated ' + $(this).html());
+        });
 });
